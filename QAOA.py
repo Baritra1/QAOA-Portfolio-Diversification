43bff0308cfbe85bb386a37cb3be87eade1995e9
Baritra1 <aritraandbanerjee@gmail.com>
2024-08-05 16:23:01 -0400

    modify logic

--- QAOA.py
+++ QAOA.py
@@ -11,8 +11,8 @@ import math
 
 
 
-#Create quantum circuit for QAOA from edges and parmeters
-def QAOA(n,edges,p,betas,gammas):  
+#Create quantum circuit for QAOA from parameters
+def QAOA(n,q,p,betas,gammas):  
     #Define quantum and classical registers
     nQubits=n**2+n
     qr = QuantumRegister(nQubits)
@@ -28,23 +28,19 @@ def QAOA(n,edges,p,betas,gammas):
         #cost hamiltonian
         for i in range(n):
             for j in range(n):
-                        qubit=n*i+j
-                        circuit.z(qubit)
-                        circuit.rz(phi=gammas[P]*correlation_matrix[i][j],qubit=qubit)
-                        circuit.z(qubit)
+                    qubit=n*i+j
+                    circuit.rz(phi=gammas[P]*correlation_matrix[i][j],qubit=qubit)
 
-        penalty=3*maxEdgeWeight
+        penalty=10*maxEdgeWeight
         #generate penalty unitary matrices 
 
         #choosing multiple most similar stocks
         entries= [1]*(2**n)
         cur=0
-        count=0
         while cur<2**n:
-            if(count.bit_count()!=1):
+            if(cur.bit_count()!=1):
                 entries[cur]=0
-            cur+=n+1
-            count+=1
+            cur+=1
         for i in range(len(entries)):
             entries[i]=math.e**(entries[i]*gammas[P]*penalty*1j)
         for i in range(n):
@@ -54,12 +50,10 @@ def QAOA(n,edges,p,betas,gammas):
         #choosing the wrong amount of clusters 
         entries= [1]*(2**n)
         cur=0
-        count=0
         while cur<2**n:
-            if(count.bit_count()!=q):
+            if(cur.bit_count()!=q):
                 entries[cur]=0
-            cur+=n+1
-            count+=1
+            cur+=1
         for i in range(len(entries)):
             entries[i]=math.e**(entries[i]*gammas[P]*penalty*1j)
         wrongCluster = list(range(n**2,n**2+n))
@@ -73,23 +67,23 @@ def QAOA(n,edges,p,betas,gammas):
             similarCluster=[n*j+j,n**2+j]
             circuit.append(DiagonalGate(entries),similarCluster)
 
-        # #x_{ij}<=y_j
-        # entries=[1,1,0,1]
-        # for i in range(len(entries)):
-        #     entries[i]=math.e**(entries[i]*gammas[P]*penalty*1j)
-        # for i in range(n):
-        #     for j in range(n):
-        #         consistencyConstraint=[n*i+j,n**2+j]
-        #         circuit.append(DiagonalGate(entries),consistencyConstraint)
+        #x_{ij}<=y_j
+        entries=[1,1,0,1]
+        for i in range(len(entries)):
+            entries[i]=math.e**(entries[i]*gammas[P]*penalty*1j)
+        for i in range(n):
+            for j in range(n):
+                consistencyConstraint=[n*i+j,n**2+j]
+                circuit.append(DiagonalGate(entries),consistencyConstraint)
 
         #mixer hamiltonian
         for q in range(nQubits):
             circuit.rx(theta=2*betas[P],qubit=q)   
     circuit.measure(qr,cr)
+    # circuit.draw(filename="circuit.png",output="mpl",scale=0.25)
     return circuit
 
-#Compute all scores for a set of edges
-def computeExpectationValue(counts,edges,n):
+def computeExpectationValue(counts,q,n):
     totalScore = 0
     totalSamples = 0
     #For each bitstring measured (keys in counts dictionary)
@@ -101,7 +95,7 @@ def computeExpectationValue(counts,edges,n):
                     score+=correlation_matrix[i][j]
 
         #penalty for violating constraints
-        penalty=3*maxEdgeWeight
+        penalty=10*maxEdgeWeight
         substrings=[bitstring[i:i + n] for i in range(0, len(bitstring)-n, n)]
         clusters=bitstring[n**2:n**2+n]
 
@@ -123,16 +117,15 @@ def computeExpectationValue(counts,edges,n):
             if count!=1:
                 score-=penalty
 
-        # #apply penalty for x_{ij}>y_j
-        # for i in range(n):
-        #     for j in range(n):
-        #         if bitstring[n*i+j]>bitstring[n**2+j]:
-        #             score-=penalty
+        #apply penalty for x_{ij}>y_j
+        for i in range(n):
+            for j in range(n):
+                if bitstring[n*i+j]>bitstring[n**2+j]:
+                    score-=penalty
 
         for j in range(n):
             if(bitstring[n*j+j]!=bitstring[n**2+j]):
                 score-=penalty
-
         totalScore += score * counts[bitstring]  #Multiply score times the # of times it was observed
         totalSamples += counts[bitstring]        #Keep track of the number of measurements (samples)
     print("Cost: "+str(totalScore))
@@ -164,17 +157,18 @@ def ExpectationValue(params):
     score = computeExpectationValue(counts,q,n)
     return(-score)
 
-def optimize(n,q,p,nIterations,nSamples,params,rhobeg):
+def optimize(nIterations,params,rhobeg):
     out = minimize(ExpectationValue,x0=params,method="COBYLA",options={'maxiter':nIterations,'rhobeg':rhobeg})
     print(f'Out: {out}')
     optimal_params = out['x'] 
     counts=runCKT(optimal_params)
+    
     # plt.bar(list(counts.keys()),list(counts.values()),width=0.9)
     # plt.xlabel("bitstrings")
     # plt.ylabel("counts")
     # plt.title("Results")
     # plt.show()
-    return max(zip(counts.values(), counts.keys()))[1]
+    return sorted(zip(counts.values(), counts.keys()))
 
 
 #4 stocks
@@ -190,7 +184,7 @@ nIterations = 10000
 #Typically need quite a few samples (measurements of quantum circuit) per iteration to 
 nSamples = 10000
 #initial trust
-rhobeg=1000.0
+rhobeg=100000.0
 #choose parameters near 0
 params=[]
 maxEdgeWeight=0
@@ -199,4 +193,8 @@ for i in range(len(correlation_matrix)):
         maxEdgeWeight=max(maxEdgeWeight,correlation_matrix[i][j])
 for i in range(p*2):
     params.append(0.01*np.random.rand())
-print("Best bitstring:",optimize(n=n,q=q,p=p,nIterations=nIterations,nSamples=nSamples,params=params,rhobeg=rhobeg))
+answer =optimize(nIterations=nIterations,params=params,rhobeg=rhobeg)
+for i in range(5):
+    print(i,answer[i])
+for i in range(10):
+    print(-i,answer[-i])
